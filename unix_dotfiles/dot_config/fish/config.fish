2ef9239ac953742a53013846b9de0f75d1f4e999
Andrij König <47005148+andikon@users.noreply.github.com>
2026-04-05 15:26:25 +0200

    simplify fish

--- unix_dotfiles/dot_config/fish/config.fish
+++ unix_dotfiles/dot_config/fish/config.fish
@@ -1,7 +1,3 @@
-# -----------------------------
-# Minimal Fish Config
-# -----------------------------
-
 # Environment
 set -gx EDITOR nvim
 set -gx VISUAL nvim
@@ -10,17 +6,10 @@ set -gx TERM tmux-256color
 set -gx XDG_CONFIG_HOME $HOME/.config
 
 # Add local bin to PATH
-fish_add_user_paths $HOME/.local/bin
+fish_add_path $HOME/.local/bin
 
-# Key bindings (emacs-style like your zsh)
 fish_default_key_bindings
 
-bind \cp history-search-backward
-bind \cn history-search-forward
-
-# Aliases (minimal core)
-alias ls "eza --long --no-user --header --icons --git --all --group-directories-first"
-alias tree "eza --tree --long --icons"
 alias v nvim
 alias g git
 alias t tmux
